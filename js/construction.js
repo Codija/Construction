3c6a4bbe6f9ce9a3fe66297d8e90b9271e471e5a
Codija <pvlvcdavid@gmail.com>
2017-03-14 12:08:06 +0100

    Added jquery slideDown effect

--- js/construction.js
+++ js/construction.js
@@ -1,16 +1,21 @@
-var today = new Date();
-var hoursNow = today.getHours();
-var message;
+$(document).ready(function() {
+    $(".main").slideDown(400);
+    $(".gallery").slideDown(400);
+    $(".mainContact").slideDown(400);
+    var today = new Date();
+    var hoursNow = today.getHours();
+    var message;
 
-if ( hoursNow > 18) {
-    message = 'Good evening!';
-} else if (hoursNow > 12) {
-    message = 'Good afternoon!';
-} else if (hoursNow > 0) {
-    message = 'Good morning';
-} else {
-    message = 'Welcome!';
-}
+    if ( hoursNow > 18) {
+        message = 'Good evening!';
+    } else if (hoursNow > 12) {
+        message = 'Good afternoon!';
+    } else if (hoursNow > 0) {
+        message = 'Good morning';
+    } else {
+        message = 'Welcome!';
+    }
 
-var por = document.getElementById('message');
-por.textContent = message;
+    var por = document.getElementById('message');
+    por.textContent = message;
+});
